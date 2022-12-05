cfb93137a8e2e4020ce92deb09bb806256be98bd
dricocosta <cfadriano@outlook.com>
2022-12-05 19:43:23 -0300

    Analisador de texto

--- ex022 - Analisador de texto.py
+++ ex022 - Analisador de texto.py
@@ -6,8 +6,11 @@
 nome = str(input('Digite o nome completo: ')).strip()
 print('Analisando o nome completo....')
 print('Seu nome em maiúsculas é {}.'.format(nome.upper()))
-print('Seu nome possui em minusculas é {}.'.format(nome.lower()))
+print('Seu nome em minusculas é {}.'.format(nome.lower()))
 
 print('Seu nome tem ao todo {} letras.'.format(len(nome)-nome.count(' ')))
 
-print('Seu primeiro nome tem {} letras.'.format(nome.find(' ')))
+#print('Seu primeiro é {} e tem {} letras.'.format(nome, nome.find(' ')))
+
+separa = nome.split()
+print('Seu primeiro nome é {} e tem {} letras.'.format(separa[0], len(separa[0])))
