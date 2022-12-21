02a63689b562e4650ec21accea8df32477ac88f2
dricocosta <cfadriano@outlook.com>
2022-12-20 21:06:03 -0300

    Soma de ímpares múltiplos de três utilizando laço FOR

---
+++ ex048 - Soma ímpares múltiplos de três.py
@@ -0,0 +1,10 @@
+'''FAça um programa que calcule a soma entre os números ímpares que são
+múltiplos de três e que se encontram no intervalo de 1 até 500.'''
+
+soma = 0
+contador = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma = soma + c
+        contador = contador + 1
+print('A soma de todos os {} valores ímpares é {}.'.format(contador, soma))
