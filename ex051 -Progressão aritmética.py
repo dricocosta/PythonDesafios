8bdcc7d94464f98d543482a0b180f90bd3a35a37
dricocosta <cfadriano@outlook.com>
2022-12-26 20:48:20 -0300

    Progressão aritmética com FOR

--- ex051 -Progressão aritmética.py
+++ ex051 -Progressão aritmética.py
@@ -3,7 +3,7 @@ Aritmética. No final, mostre os 10 primeiros termos dessa progressâo.'''
 
 primeiro = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão da progressão: '))
-decimo = primeiro +(10 - 1) * razao
+decimo = primeiro + (10 - 1) * razao
 for c in range(primeiro, decimo + razao, razao):
     print('{}'.format(c), end=' -> ')
 print('Fim.')
