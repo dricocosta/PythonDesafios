f70bfc18d455e58e782993fa9c25a59cb2828915
dricocosta <cfadriano@outlook.com>
2022-12-27 18:27:02 -0300

    Adicionando algorítmo para contar vários valores

--- ex064 - Tratando valores.py
+++ ex064 - Tratando valores.py
@@ -1,9 +1,14 @@
 '''Crie um programa que leia vários números inteiros pelo teclado.
-O programa só vai para quando o usuário digitar o valor 999, que é
+O programa só vai parar quando o usuário digitar o valor 999, que é
 a condição de parada. No final, mostre quantos números foram digitados
 e qual foi a soma entre eles (desconsiderando o flag).'''
 
 n = 0
+contador = 0
+soma = 0
+n = int(input('Digite um número [999 PARA PARAR]: '))
 while n != 999:
-    n = int(input('Digite um número: '))
-print(n)
+    contador = contador + 1
+    soma = soma + n
+    n = int(input('Digite um número [999 PARA PARAR]:  '))
+print('Você digitou {} números e a soma entre eles foi {}.'.format(contador, soma))
